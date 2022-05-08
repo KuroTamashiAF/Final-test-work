ae213ed75cc612c95717bbe20fd2e3470566decd
Alex <chugun777akk@mail.ru>
2022-05-08 11:37:15 +0700

    add block diagram

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Myprogramm
+{
+    class ProgrammMY
+    {
+        private static void Main(string[] args)
+        {
+        
+        }
+    }
+}
