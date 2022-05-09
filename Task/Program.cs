77b1b440b7cdb95d046670d1b7264f5a3d0181a9
Alex <chugun777akk@mail.ru>
2022-05-09 13:02:02 +0700

    modified block diagram

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 namespace Myprogramm
 {
-    class ProgrammMY
+    class Programm
     {
+        public static void FillArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+        }
+        public static void PrintArray(string[] arr)
+        {
+            Console.Write("[");                                
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.Write("]");                                
+        }
+        public static string[] SelectionOfElements(string[] ArrayInput)
+        {
+            int CountElementNewArray = 0;
+            for (int i = 0; i < ArrayInput.Length; i++)
+            {
+                if (ArrayInput[i].Length <= 3)
+                {
+                    CountElementNewArray++;
+                }
+            }
+            string[] ResultArray = new string[CountElementNewArray];
+            int k = 0;                                                            
+            for (int j = 0; j < ArrayInput.Length; j++)
+            {
+                if (ArrayInput[j].Length <= 3)
+                {
+                    ResultArray[k] = ArrayInput[j];
+                    k++;
+                }
+            }
+            return ResultArray;
+        }
         private static void Main(string[] args)
         {
-        
+            int size = 5;
+            string[] Array = new string[size];
+            FillArray(Array);
+            PrintArray(Array);
+            string[] TotalResultArray = SelectionOfElements(Array);
+            Console.WriteLine();                                              
+            PrintArray(TotalResultArray);
         }
     }
 }
